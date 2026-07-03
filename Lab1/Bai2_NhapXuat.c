f9fc685da4a2260bea6bc1e9f8ec7d5979a94886
Vochethien <karakurito2005@gmail.com>
2026-07-04 01:58:43 +0700

    Lab1 - Bai2

--- Lab1/Bai2_NhapXuat.c
+++ Lab1/Bai2_NhapXuat.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main(){
-    short t,l,h;
-    float tb;
-    t = 7;
-    l = 8;
-    h = 10;
-    tb = 1.0*(t+l+h)/3; //ép kiểu
-    printf("Toan: %d Ly: %d Hoa: %d \n",t,l,h);
-    printf("Diem trung binh: %f\n",tb);
+    short t,l,h;//chon kieu short vi la so nguyen nho
+    float tb;//chon kieu float vi la so thap phan. "tb" la ten bien
+    char hoTen[50];//chu dau tien viet thuong, ky tu dau tien cua chu tiep theo viet hoa. 
+    //Boi vi ten bien khong co khoang trang
+    char MSSV[10];
+    unsigned short Namsinh;
+    float DiemTB; 
+    printf("Nhap ho va ten: ");//. Xuat thong tin trong dau " "
+    scanf("%[^\n]",hoTen);
+
+    printf("Nhap MSSV: ");
+    scanf("%s",MSSV);
+
+    printf("Nhap nam sinh: "); 
+    scanf("%hu", &Namsinh);
+
+    printf("Nhap diem trung binh: ");
+    scanf("%f", &DiemTB);
+
+    printf("Thong tin sinh vien:\n");
+    printf("Ho va ten: %s\n", hoTen);//%s la ky tu chuoi
+    printf("MSSV: %s\n", MSSV);
+    printf("Nam sinh: %hu\n", Namsinh);//%hu la kieu so nguyen khong dau
+    printf("Tuoi: %d\n", 2026 - Namsinh);
+    printf("Diem trung binh: %.2f\n", DiemTB);//%.2f la kieu so thap phan co 2 chu so sau dau cham
     return 0;
 }
